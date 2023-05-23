b6fcb6b3ed7c103a611764316c7c438c99909c9b
VictorStankov <viktorstankov1@gmail.com>
2023-05-24 02:01:18 +0300

    feat: Add commands used for interaction with the bot
    Signed-off-by: VictorStankov <viktorstankov1@gmail.com>

---
+++ bot/bot_commands.py
@@ -0,0 +1,102 @@
+from typing import Union
+
+from discord import VoiceClient
+from discord.ext import commands
+from discord.ext.commands import Bot, Context
+
+from bot.model.music_controller import MusicController
+
+
+class BotCommands(commands.Cog):
+    """
+    Class to encapsulate all commands that users can call.
+    """
+    def __init__(self, client: Bot):
+        self.client = client
+
+    @staticmethod
+    async def get_voice_channel(ctx: Context) -> Union[VoiceClient, None]:
+        """
+        Static method to retrieve the channel the bot is currently in. If it is not joined in any channel, it tries to
+        connect to the caller's channel.
+
+        :param ctx: Context
+        :return: VoiceClient, None
+        """
+        channel: Union[VoiceClient, None] = ctx.guild.voice_client
+
+        if channel is not None and channel.is_connected() is True:
+            return channel
+        elif ctx.author.voice is not None:
+            return await ctx.author.voice.channel.connect(self_deaf=True)
+        return None
+
+    @commands.command()
+    async def join(self, ctx: Context) -> None:
+        """
+        Command that makes the bot join the user's channel and play the iconic theme song. It makes checks whether it
+        has already joined a channel or the user is not present in any voice channels.
+
+        :param ctx: Context
+        :return: None
+        """
+        if ctx.guild.voice_client:
+            await ctx.reply("I'm already connected! :rage:")
+            return
+
+        vc = await BotCommands.get_voice_channel(ctx=ctx)
+
+        if not vc:
+            await ctx.reply("Voice channel not found! :angry:")
+            return
+
+        await MusicController.play_intro_song(ctx=ctx)
+
+    @commands.command()
+    async def play(self, ctx: Context, *args) -> None:
+        """
+        Similar to `join` method. If the bot has not already joined, it joins the user's channel. After that it hands
+        the control over to MusicController to start the music clip. After successfully executing, it reacts with a
+        checkmark to show the user their request has been serviced.
+
+        :param ctx: Context
+        :param args: Music info - can be URL or title of the song
+        :return: None
+        """
+        vc = await BotCommands.get_voice_channel(ctx=ctx)
+
+        if not vc:
+            await ctx.reply("Voice channel not found! :angry:")
+            return
+
+        await MusicController.add_to_queue(ctx, ' '.join(args))
+        await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
+
+    @commands.command(aliases=['dc', 'disconnect'])
+    async def leave(self, ctx: Context) -> None:
+        """
+        Method used for leaving the voice channel. It checks whether the bot is present in any of the voice channels and
+        tells MusicController to proceed with destroying the player for the channel.
+
+        :param ctx: Context
+        :return: None
+        """
+        if ctx.guild.voice_client:
+            await MusicController.destroy_player(ctx=ctx)
+            await ctx.guild.voice_client.disconnect(force=False)
+        else:
+            await ctx.reply("I'm not even connected! :triumph:")
+
+    @commands.command()
+    async def stop(self, ctx: Context) -> None:
+        """
+        Method used for stopping the music player. It checks whether the bot is present in any of the voice channels and
+        tells MusicController to proceed with killing the process of the player and clearing its queue.
+
+        :param ctx: Context
+        :return: None
+        """
+        if ctx.guild.voice_client:
+            await MusicController.stop_playing(ctx=ctx)
+        else:
+            await ctx.reply("I'm not even connected! :triumph:")
